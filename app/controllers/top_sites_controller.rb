84649b50e0dc5b4436a56d2eb088014f22fe0088
Bashanta Dahal <basantad2@gmail.com>
2021-07-06 20:02:44 -0700

    displaying stock ticker

--- app/controllers/top_sites_controller.rb
+++ app/controllers/top_sites_controller.rb
@@ -1,9 +1,9 @@
 class TopSitesController < ApplicationController
 	def index
-		stock_names = Stock.all.collect(&:display_website).compact
-		top_sites = TopSite.all.where(:company_url => stock_names)
+		stock_urls = Stock.all.collect(&:display_website).compact
+		top_sites = TopSite.all.where(:company_url => stock_urls)
 		@histories = SiteHistory.where(:top_site_id => top_sites.collect(&:id)).sort do |a, b|
 			a.abs_percentage <=> b.abs_percentage
-		end.select { |a| a.end_rank > a.start_rank }.reverse
+		end.select { |a| a.end_rank < a.start_rank  && a.abs_percentage > 20 && a.start_rank < 50000}.reverse
 	end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,5 +3,16 @@ class Stock < ApplicationRecord
 
 	def display_website
 		website.to_s.gsub("https://", "").gsub("www.", "").split("/").first
-	end	
+	end
+
+	def self.link_top_site
+		Stock.all.map do |stock|
+			if stock.display_website.present?
+				corresponding_site = TopSite.where(:company_url => stock.display_website).first
+				if corresponding_site.present?
+					corresponding_site.update(:stock_id => stock.id)
+				end
+			end
+		end
+	end
 end
